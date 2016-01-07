d2e4fa46c87d7e77d7db788e8442c0f8332a7c9a
jasonczc <jasonczc@qq.com>
2016-01-07 11:20:59 +0800

    完善create，query项目

--- YDAuth/src/YDAuth/task.php
+++ YDAuth/src/YDAuth/task.php
@@ -6,14 +6,15 @@ class task extends AsyncTask{
 	
 	private $plugin;
 	
-	public function __construct($pluginname,$db,$mode,$k1,$k2,$v,$cb)
+	public function __construct($pluginname,$db,$table,$mode,$k1,$k2,$v,$cb)
 	{
 	    $this->pluginname=$pluginname;//插件名称
 		$this->db=$db;//mysql数据库
+		$this->table = $table;//table名
 		$this->mode=$mode;//查询模式
 		$this->k1=$k1;//键值1：通指玩家名
 		$this->k2=$k2;//键值2：赋值2
-		$this->v=$v;//表名
+		$this->v=$v;//值
 		$this->cb=$v;//回传function名称
 	}
 	
@@ -21,10 +22,36 @@ class task extends AsyncTask{
 	{
 	    switch($this->mode){
 	        case "query";
+	        $this->setResult=($this->db->query("SELECT * FROM ".$this->table." where ID = '".$this->k1."'"));
 	        break;
-	        case "set";
+	        case "set"://支持：单变量与数组
 	        break;
 	        case "create";
+	   foreach($this->v as $k=>$v){
+	       if(isset($表名)){
+	           $表名=$表名.",".$k;
+	       }else{
+	           $表名=$k;
+	       }
+	       if(isset($值)){
+	           $值=$值.",'".$k."'";
+	       }else{
+	           $表名="'".$k."'";
+	       }
+	   }
+	   $this->db->query = "insert into ".$this->table."(".$表名.") values(".$值.")";
+		/*
+		传入$this->v格式：
+		$this->v = array(
+		"a"=>"va",
+		"b"=>"vb",
+		"c"=>"vb\c",
+		"d"=>"vd"
+				)；
+				相当于：
+		$sql = "insert into ".$this->table."(a,b,c,d) values('va','vb','vc','vd')";
+		*/
+		$this->Setresult(true);
 	        break;
 	    }
 	}
