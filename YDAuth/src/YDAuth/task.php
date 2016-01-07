4ed65fd8cd3a89b00afb74cb91a874ea7b51ad73
jasonczc <jasonczc@qq.com>
2016-01-07 11:02:35 +0800

    全部重写，没写完

--- YDAuth/src/YDAuth/task.php
+++ YDAuth/src/YDAuth/task.php
@@ -1,25 +1,36 @@
 <?php
 namespace YDAuth;
-use pocketmine\scheduler\PluginTask;
-class task extends PluginTask{
+use pocketmine\Server;
+use pocketmine\scheduler\AsyncTask;
+class task extends AsyncTask{
 	
 	private $plugin;
 	
-	public function __construct(YDAuth $plugin,\mysqli $db)
+	public function __construct($pluginname,$db,$mode,$k1,$k2,$v,$cb)
 	{
-		$this->plugin=$plugin;
-		$this->db=$db;
-		parent::__construct($plugin);
+	    $this->pluginname=$pluginname;//插件名称
+		$this->db=$db;//mysql数据库
+		$this->mode=$mode;//查询模式
+		$this->k1=$k1;//键值1：通指玩家名
+		$this->k2=$k2;//键值2：赋值2
+		$this->v=$v;//表名
+		$this->cb=$v;//回传function名称
 	}
 	
-	public function onRun($ck)
+	public function onRun()
 	{
-		$status=$this->db->ping();
-		$this-plugin->status=$stauts;
-		if($status == false)
-		{
-			$this->main->MysqlConnect();
-		}
+	    switch($this->mode){
+	        case "query";
+	        break;
+	        case "set";
+	        break;
+	        case "create";
+	        break;
+	    }
+	}
+	public function onCompletion(Server server){
+	    $爆炸=$this->cb;
+	   Server::getInstance()->getPlugin($this->pluginname)->$爆炸($k1,$this->getResult());//回传function请给出两个参数
 	}
 	
 }
