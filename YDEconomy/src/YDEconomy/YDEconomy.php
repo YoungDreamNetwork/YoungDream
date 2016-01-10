e1428b7f1ab318142075bbbd64bf2601c89eb4b5
labi <1292504237@qq.com>
2016-01-10 12:29:19 +0800

    YDEco加入兑换码

--- YDEconomy/src/YDEconomy/YDEconomy.php
+++ YDEconomy/src/YDEconomy/YDEconomy.php
@@ -10,18 +10,29 @@ use pocketmine\Player;
 use pocketmine\Server;
 use pocketmine\plugin\Plugin;
 use pocketmine\event\player\PlayerJoinEvent;
-use pocketmine\scheduler\PluginTask;
 
 class YDEconomy extends PluginBase implements Listener{
 	
-	
+	public $plist=[];
+	public $instance;
+	public $money;
+	public $clist=[];
+	public $csave;
+
 	public static function getInstance(){
 		return self::$instance;
 	}
 	public function onLoad(){
 		self::$instance = $this;
 	}
-	
+	 public function onDisable()
+	 {
+		 $this->csave->setAll($this->clist);
+		 $this->csave->save();
+		 $this->money->setAll($this->plist);
+		 $this->money->save();
+
+	 }
 	
 	public function onEnable(){
 		$this->getLogger()->info("YDEconomy 加载中!");
@@ -29,7 +40,9 @@ class YDEconomy extends PluginBase implements Listener{
 		$this->getServer()->getPluginManager()->registerEvents($this,$this);
 		
 		$this->money = new Config($this->getDataFolder() . "money.yml", Config::YAML, array());
-		$this->plist=$money->getAll();
+		$this->csave = new Config($this->getDataFolder()."card.yml",Config::YAML,array());
+		$this->plist=$this->money->getAll();
+		$this->clist=$this->csave->getAll();
 		$this->getLogger()->info("YDEconomy加载完毕");	
 	}
 	
@@ -39,22 +52,24 @@ class YDEconomy extends PluginBase implements Listener{
 		if(!isset($n))
 		{
 			$n=0;
-			return;
+			return true;
 		}
 		$n=$n+1;
 		if($n>50)
 		{
 			$this->money->setAll($this->plist);
+			$this->money->save();
 			return true;
 		}
 		return true;
 	}
-	
-	
+
+
+
 	public function onPlayerJoin(PlayerJoinEvent $event){
 		$player = $event->getPlayer();
 		$name = $player->getName();
-		if(!in_array($name,$this->plist)
+		if(!in_array($name,$this->plist))
 		{
 			$this->CreatePlayer($name,0);
 			$player->sendMessage("[梦灵]欢迎来到轻梦，您当前有 0 梦核。");
@@ -63,58 +78,99 @@ class YDEconomy extends PluginBase implements Listener{
 		$pmoney=$this->SeeMoney($name);
 		$player->sendMessage("[梦灵]欢迎回来，您当前有 $pmoney 梦核。");
 	}
-	
+
+	/**
+	 * @param $name
+	 * @return int
+     */
 	public function SeeMoney($name){
 		$this->save();
-		if(!in_array($name,$this->plist)
+		if(!in_array($name,$this->plist))
 		{
 			$this->CreatePlayer($name,0);
 			return 0;
 		}
+
 		return $this->plist[$name];
 	}
-	
-	public function SetMoney($name,$money){
+
+	/**
+	 * @param $name
+	 * @param $money
+	 * @return int
+     */
+	public function SetMoney($name, $money){
 		$this->save();
-		if(!in_array($name,$this->plist)
+		if(!in_array($name,$this->plist))
 		{
 			$this->CreatePlayer($name,0);
 			return 0;
 		}
 		return $this->plist[$name]=$money;
 	}
-	
-	public function CreatePlayer($name,$money){
+
+
+	/**
+	 * @param $name
+	 * @param $money
+	 * @return bool
+     */
+	public function CreatePlayer($name, $money){
 		$this->save();
-		if(!in_array($name,$this->plist)
+		if(!in_array($name,$this->plist))
 		{
 			return false;
 		}
-		return $this->plist[$name]=$money;
+		$this->plist[$name]=$money;
+		return $this->plist[$name];
 	}
-	
-	public function AddMoney($name,$money)
+
+	/**
+	 * @param $name
+	 * @param $money
+	 * @return bool
+     */
+	public function AddMoney($name, $money)
 	{
 		$this->save();
-		if(!in_array($name,$this->plist)
+		if(!in_array($name,$this->plist))
 		{
 			$this->CreatePlayer($name,0);
 			return false;
 		}
-		return $this->plist[$name]=$this->plist[$name]+$money;
+		$this->plist[$name]=$this->plist[$name] + $money;
+		return $this->plist[$name];
 	}
-	
-	public function ReduceMoney($name,$money)
+
+	/**
+	 * @param $name
+	 * @param $money
+	 * @return bool
+     */
+	public function ReduceMoney($name, $money)
 	{
 		$this->save();
-		if(!in_array($name,$this->plist)
+		if(!in_array($name,$this->plist))
 		{
 			$this->CreatePlayer($name,0);
 			return false;
 		}
-		return $this->plist[$name]=$this->plist[$name]-$money;
+		$this->plist[$name]=$this->plist[$name]-$money;
+		return $this->plist[$name];
 	}
-	
+
+	public function card($name,$pass)
+	{
+		if(in_array($pass,$this->clist))
+		{
+			$this->AddMoney($name,$this->clist[$pass]);
+			unset($this->clist[$pass]);
+			return $this->clist[$pass];
+
+		}
+		return false;
+	}
+
 	public function onCommand(CommandSender $sender, Command $command, $label, array $args)
 		{
 			$name = $sender->getName();
@@ -166,6 +222,20 @@ class YDEconomy extends PluginBase implements Listener{
 							return true;	
 					}
 					$sender->sendMessage("请输入正确的格式/setmoney <玩家名> <数量>");
+				break;
+
+				case "card":
+					if(isset($args[0]) and is_numeric($args[0]))
+					{
+						$result=$this->card($name,$args[0]);
+						if($result != false)
+						{
+							$now= $this->SeeMoney($name);
+							$sender->sendMessage("[梦灵]已成功兑换 $result 梦核，你现在拥有 $now 梦核");
+							return true;
+						}
+						$sender->sendMessage("[梦灵]兑换失败，请检查梦核兑换码是否正确。");
+					}
 				break;
 
 			}
